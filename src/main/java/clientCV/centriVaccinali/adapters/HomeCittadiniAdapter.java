e0551c79fe56af201ecb96e83b2fb901c9d87c70
Fernando Edelstein <fernandoedelstein99@gmail.com>
2021-06-21 16:14:07 +0200

    Solved LogIn Field Issue

--- src/main/java/clientCV/centriVaccinali/adapters/HomeCittadiniAdapter.java
+++ src/main/java/clientCV/centriVaccinali/adapters/HomeCittadiniAdapter.java
@@ -1,4 +1,39 @@
 package clientCV.centriVaccinali.adapters;
 
-public class HomeCittadiniAdapter {
+import clientCV.shared.Utente;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
+
+public class HomeCittadiniAdapter extends Adapter {
+
+    @FXML
+    private Text welcomeTextField;
+    @FXML
+    private Button btnLogout, btnRegistrati;
+
+    private Utente utente;
+
+    public void switchToCercaScene(ActionEvent event) throws IOException {
+        cambiaSchermataConUtente("Cerca.fxml", utente, event);
+    }
+
+    @Override
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+        if (utente == null) {
+            welcomeTextField.setText("Accesso come ospite");
+            btnRegistrati.setDisable(false);
+            btnLogout.setText("Accedi");
+        }
+        else {
+            welcomeTextField.setText("Ciao, " + utente.getUsername());
+            btnRegistrati.setDisable(true);
+            btnLogout.setText("Logout");
+        }
+
+    }
 }
