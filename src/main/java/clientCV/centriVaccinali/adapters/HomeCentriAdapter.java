098735db71101bd4e886d3b60ca780e920994a04
fernandoedelstein <fmedelsteinfernand@studenti.uninsubria.it>
2021-07-24 11:43:26 +0200

    Minor Adjs

--- src/main/java/clientCV/centriVaccinali/adapters/HomeCentriAdapter.java
+++ src/main/java/clientCV/centriVaccinali/adapters/HomeCentriAdapter.java
@@ -3,17 +3,21 @@ package clientCV.centriVaccinali.adapters;
 import clientCV.shared.Utente;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
-
 import java.io.IOException;
 
 public class HomeCentriAdapter extends Adapter {
+    public Button logoutBtn;
     private Utente utente;
+
     @FXML
     private Text benvenutoText;
+    @FXML
+    private Button registraCentroBtn, registraVaccinatoBtn;
 
     public void vaiARegistraCentroScene(ActionEvent event) throws IOException {
-        cambiaSchermataConUtente("RegistraCentro.fxml", utente, event);
+        cambiaSchermataConUtente("RegistraCentroVaccinale.fxml", utente, event);
     }
 
     public void vaiARegistraVaccinatoScene(ActionEvent event) throws IOException {
