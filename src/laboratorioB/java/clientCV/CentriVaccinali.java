737c2af18bbec94025c9a2946141c0679c06ff7e
Fernando Edelstein <fernandoedelstein99@gmail.com>
2021-08-20 18:43:16 +0200

    Jar Files

--- src/laboratorioB/java/clientCV/CentriVaccinali.java
+++ src/laboratorioB/java/clientCV/CentriVaccinali.java
@@ -16,7 +16,7 @@ public class CentriVaccinali extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("../Layout/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/Layout/Login.fxml"));
 
         Scene scene = new Scene(root, 900, 600);
         scene.getRoot().setStyle("-fx-font-family: 'serif'");
